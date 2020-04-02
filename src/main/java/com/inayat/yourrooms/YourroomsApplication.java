ab365d144f1a9fb2dee1ac26ebdfd8580b45529b
Inayat Hussain <inayat>
2020-04-02 14:01:16 +0530

    fix

--- src/main/java/com/inayat/yourrooms/YourroomsApplication.java
+++ src/main/java/com/inayat/yourrooms/YourroomsApplication.java
@@ -2,13 +2,19 @@ package com.inayat.yourrooms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class YourroomsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(YourroomsApplication.class, args);
-	}
-}
+public class YourroomsApplication extends SpringBootServletInitializer {  
+	@Override  
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)   
+	{  
+	return application.sources(YourroomsApplication.class);  
+	}  
+	public static void main(String[] args)   
+	{  
+	SpringApplication.run(YourroomsApplication.class, args);  
+	}  }

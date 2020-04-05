07742ed4efe057f8d6427687d95ec7d05a06a328
itsin <inayat>
2020-04-05 14:28:27 +0530

    test

--- src/main/java/com/inayat/yourrooms/security/SecurityConfig.java
+++ src/main/java/com/inayat/yourrooms/security/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers("/user/mobile-login").permitAll()
             .antMatchers("/user/testAuth/**").hasRole("ADMIN")
             .antMatchers("/consumer").hasRole("CONSUMER")
-            .antMatchers("/update-profile").hasRole("CONSUMER")
+            .antMatchers("/update-profile").permitAll()
             
             
             

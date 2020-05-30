e5bbe6661577f75f591514639df788fd1ad62d4a
itsin <inayat>
2020-05-30 12:15:37 +0530

    response changed

--- src/main/java/com/inayat/yourrooms/model/ApiResponse.java
+++ src/main/java/com/inayat/yourrooms/model/ApiResponse.java
@@ -1,5 +1,8 @@
 package com.inayat.yourrooms.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -8,47 +11,47 @@ public class ApiResponse {
 
 	private int statusCode;
 	private String message;
-	private Object data;
+	private List<Object> data;
 
-	public ApiResponse(int status, String message, Object data){
-	    this.statusCode = status;
-	    this.message = message;
-	    this.data = data;
-    }
-
-    public ApiResponse(int status, String message){
-        this.statusCode = status;
-        this.message = message;
-    }
-
-    public int getStatus() {
-        return statusCode;
-    }
-
-    public void setStatus(int status) {
-        this.statusCode = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-   
-
-    @Override
-	public String toString() {
-		return "ApiResponse [statusCode=" + statusCode + ", message=" + message +"]";
+	public ApiResponse(int status, String message, Object data) {
+		this.statusCode = status;
+		this.message = message;
+		List<Object> datalist = new ArrayList<>();
+		datalist.add(data);
+		this.data = datalist;
 	}
 
-	public Object getData() {
+	public ApiResponse(int status, String message) {
+		this.statusCode = status;
+		this.message = message;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "ApiResponse [statusCode=" + statusCode + ", message=" + message + "]";
+	}
+
+	public List<Object> getData() {
 		return data;
 	}
 
-	public void setData(Object data) {
+	public void setData(List<Object> data) {
 		this.data = data;
 	}
 
